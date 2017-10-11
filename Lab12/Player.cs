4baae5b618d3257874eeca13973b7570275c24d5
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-11 14:50:39 -0400

    First commit

---
+++ Lab12/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    abstract class Player //abstract class called Player, holds generateRoshambo method
+    {
+        //method generateRoshambo returns string
+        protected string rock;
+        protected string paper;
+        protected string scissors;
+
+
+        
+
+    }
+}
